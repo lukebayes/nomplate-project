790f048bcbb9b109a84359d705032ed340aa6a6b
Luke Bayes <lukebayes@gmail.com>
2019-01-20 18:26:24 -0500

    Added test for client dom structure

--- test/client_test.js
+++ test/client_test.js
@@ -1,17 +1,31 @@
 const assert = require('chai').assert;
-const client = require('../').client;
 const dom = require('nomplate').dom;
+const {JSDOM} = require('jsdom');
+const client = require('../').client;
+const renderElement = require('nomplate').renderElement;
 
 describe('Client', () => {
   let instance;
 
-  beforeEach(() => {
-    var fakeWindow = {};
-    instance = client(fakeWindow);
-  });
+  function render(options) {
+    const fakeWindow = new JSDOM('').window;
+    return renderElement(client(fakeWindow), fakeWindow.document);
+  }
 
   it('is instantiable', () => {
-    assert(instance);
+    assert.isNotNull(render());
+  });
+
+  it('renders header element', () => {
+    const client = render().querySelector('h1');
+    assert.equal(client.innerHTML, 'Hello World');
+  });
+
+  it('renders 2 childern', () => {
+    const client = render();
+    assert.equal(client.childNodes.length, 2);
+    assert.equal(client.childNodes[0].childNodes.length, 1);
   });
 });
+
 
